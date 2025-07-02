2cb5426393d17af93e57221bde55d159a4050224
peesrikaew <6511130046@mut.ac.th>
2025-07-02 16:26:36 +0700

    fixed on example file1

--- src/homepage.jsx
+++ src/homepage.jsx
@@ -208,9 +208,7 @@ function HomePage() {
             ["Email", "Leave Start Date", "Leave End Date", "Type Of Leave", "Site"],
             ["ndodth@gmail.com", "2024-06-01", "2024-06-03", leaveTypes[0], sites[0]],
             ["ndodth@gmail.com", "2024-06-05", "2024-06-07", leaveTypes[1], sites[0]],
-            [],
-            ["หมายเหตุ: กรอก 'Type Of Leave' ตามตัวเลือกใน Sheet2 และ Site ตามตัวเลือกใน Sheet2"]
-        ];
+           ];
 
         // Sheet2: ตัวเลือก dropdown
         const wsOptions = [
